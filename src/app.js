698c7c6dfdb124972c4eab13ada7135d144869ac
pataruco <pataruco@gmail.com>
2016-03-17 22:36:55 +0000

    Render a new table when a match is created

--- src/app.js
+++ src/app.js
@@ -8,11 +8,10 @@ $(document).ready(function(){
     { "team": { "name": "England", "id": 1 }, "pos": 4, "pts": 52.32 },
     { "team": { "name": "Romania", "id": 24 }, "pos": 5, "pts": 43.50 }
   ];
-
+  var match;
   var $table = $('#table');
 
   pulseball.init(initialRankingTable);
-  console.log(pulseball.rankingsTable);
 
   $(window).on('load', renderTable(pulseball.rankingsTable));
 
@@ -20,13 +19,11 @@ $(document).ready(function(){
 
   function renderTable(table){
     table.forEach(function(team){
-      console.log(team);
       $table.append(
-        '<tr><td>'+ team.pos + '</td><td>'+ team.team.name +'</td><td>'+ team.pts +'</td></tr>'
+        '<tr><td>'+ team.pos + '</td><td>'+ team.team.name +'</td><td>'+ team.pts.toFixed(2) +'</td></tr>'
       );
     });
   };
-
 
   function createNewMatch(e){
     e.preventDefault();
@@ -35,12 +32,36 @@ $(document).ready(function(){
     var team2 = $('#new-match input[name="team-2"]:checked').val();
     var venue = $('#new-match input[name="venue"]:checked').val();
     var outcome = $('#new-match input[name="outcome"]:checked').val();
-    console.log(team1);
-    console.log(team2);
-    console.log(venue);
-    console.log(outcome);
+    match = { "matchId": 2524,
+                    "description": "Match 2",
+                    "venue": {
+                      "id": 900,
+                      "name": "Stadium",
+                       "city": "Paris",
+                       "country": venue
+                     },
+                    "teams": [
+                      {
+                        "id": 2,
+                        "name": team2,
+                        "abbreviation": "FRA"
+                      },
+                      {
+                        "id": 1,
+                        "name": team1,
+                        "abbreviation": "ENG"
+                      }
+                    ],
+                    "scores": [
+                      19,
+                      23
+                    ],
+                    "status": "C",
+                    "outcome": outcome
+                  };
+    pulseball.addMatch(match);
+    $table.empty();
+    renderTable(pulseball.rankingsTable);
   };
 
-
-
-});
+}); //end of document
