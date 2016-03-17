82c7f88adb3a3d2f64653e2fad235396135feeb1
pataruco <pataruco@gmail.com>
2016-03-17 19:24:42 +0000

    Create function to render a table on window on load

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
 $(document).ready(function(){
-  console.log('hola');
+
   var pulseball = new PULSEBALL();
-  console.log(pulseball);
+  var initialRankingTable = [
+    { "team": { "name": "Australia", "id": 32 }, "pos": 1, "pts": 54.23 },
+    { "team": { "name": "New Zealand", "id": 62 }, "pos": 2, "pts": 54.00 },
+    { "team": { "name": "France", "id": 2 }, "pos": 3, "pts": 52.95 },
+    { "team": { "name": "England", "id": 1 }, "pos": 4, "pts": 52.32 },
+    { "team": { "name": "Romania", "id": 24 }, "pos": 5, "pts": 43.50 }
+  ];
+
+  pulseball.init(initialRankingTable);
+  console.log(pulseball.rankingsTable);
+
+  $(window).on('load', renderTable(pulseball.rankingsTable));
 
   $('#new-match').on('submit', createNewMatch);
 
   function createNewMatch(e){
     e.preventDefault();
-    console.log('hello submit');
+
     var team1 = $('#new-match input[name="team-1"]:checked').val();
     var team2 = $('#new-match input[name="team-2"]:checked').val();
     var venue = $('#new-match input[name="venue"]:checked').val();
@@ -18,6 +29,6 @@ $(document).ready(function(){
     console.log(outcome);
   };
 
-  
+
 
 });
