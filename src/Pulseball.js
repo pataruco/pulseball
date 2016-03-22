3e407531f2de08102e4195457054933ed0c0bf79
pataruco <pataruco@gmail.com>
2016-03-22 19:00:39 +0000

    Refactor

--- src/Pulseball.js
+++ src/Pulseball.js
@@ -1,29 +1,30 @@
-let PULSEBALL = function() {
-  this.rankingsTable = null;
-};
+class PULSEBALL {
+  constructor () {
+    this.rankingsTable = null;
+  }
 
-PULSEBALL.prototype.init = function(rankingJson){
-  this.rankingsTable = rankingJson;
-};
+  init (rankingJson){
+    this.rankingsTable = rankingJson;
+  }
 
-PULSEBALL.prototype.addMatch = function(match){
-  let  team1Name;
-  let  team2Name;
-  let  venue = match.venue.country;
-  let  outcome = match.outcome;
-  this.teams(match);
-  let team1rankingIndex = this.rankingIndex(this.addMatch.team1Name);
-  let team2rankingIndex = this.rankingIndex(this.addMatch.team2Name);
-  let rankingDifference = this.rateDifference(team1rankingIndex, team2rankingIndex, venue);
+  addMatch(match){
+    let  team1Name;
+    let  team2Name;
+    let  venue = match.venue.country;
+    let  outcome = match.outcome;
+    this.teams(match);
+    let team1rankingIndex = this.rankingIndex(this.addMatch.team1Name);
+    let team2rankingIndex = this.rankingIndex(this.addMatch.team2Name);
+    let rankingDifference = this.rateDifference(team1rankingIndex, team2rankingIndex, venue);
 
-  this.checkRankingDifference(rankingDifference);
+    this.checkRankingDifference(rankingDifference);
 
-  this.updateTeamScores(team1rankingIndex, team2rankingIndex, rankingDifference, outcome);
+    this.updateTeamScores(team1rankingIndex, team2rankingIndex, rankingDifference, outcome);
 
-  this.sortTable();
-};
+    this.sortTable();
+  }
 
-PULSEBALL.prototype.teams = function(match){
+  teams(match) {
     let teams = match.teams;
     for (let i = 0; i < teams.length; i++) {
       switch (teams[i].id) {
@@ -35,81 +36,81 @@ PULSEBALL.prototype.teams = function(match){
           break;
       };
     };
-};
+  }
 
-PULSEBALL.prototype.rankingIndex = function ( team ) {
-  for (let i = 0; i < this.rankingsTable.length; i++) {
-    if(this.rankingsTable[i].team.name === team){
-      return i;
+  rankingIndex(team) {
+    for (let i = 0; i < this.rankingsTable.length; i++) {
+      if(this.rankingsTable[i].team.name === team){
+        return i;
+      };
     };
-  };
-};
+  }
 
-PULSEBALL.prototype.rateDifference = function(team1Index, team2Index, venue){
-  let team1 = this.rankingsTable[team1Index];
-  let team2 = this.rankingsTable[team2Index];
+  rateDifference(team1Index, team2Index, venue) {
+    let team1 = this.rankingsTable[team1Index];
+    let team2 = this.rankingsTable[team2Index];
 
-  if(team1.team.name === venue){
-    return Math.abs(parseFloat((( this.rankingsTable[team1Index].pts + 3) - (this.rankingsTable[team2Index].pts)).toFixed(2)));
+    if(team1.team.name === venue){
+      return Math.abs(parseFloat((( this.rankingsTable[team1Index].pts + 3) - (this.rankingsTable[team2Index].pts)).toFixed(2)));
 
-  } else if (team2.team.name === venue) {
+    } else if (team2.team.name === venue) {
 
-    return Math.abs(parseFloat(((this.rankingsTable[team1Index].pts) - (this.rankingsTable[team2Index].pts + 3)).toFixed(2)));
+      return Math.abs(parseFloat(((this.rankingsTable[team1Index].pts) - (this.rankingsTable[team2Index].pts + 3)).toFixed(2)));
 
-  } else {
+    } else {
 
-    return Math.abs(parseFloat(((this.rankingsTable[team1Index].pts) - (this.rankingsTable[team2Index].pts)).toFixed(2)));
-  };
-};
+      return Math.abs(parseFloat(((this.rankingsTable[team1Index].pts) - (this.rankingsTable[team2Index].pts)).toFixed(2)));
+    };
+  }
 
-PULSEBALL.prototype.checkRankingDifference = function (rankingDifference) {
-  if (rankingDifference >= 10 ){
-    return this.addMatch.rankingDifference = 10;
+  checkRankingDifference(rankingDifference){
+    if (rankingDifference >= 10 ){
+      return this.addMatch.rankingDifference = 10;
 
-  } else if (rankingDifference <= 10) {
-    return this.addMatch.rankingDifference = -10;
+    } else if (rankingDifference <= 10) {
+      return this.addMatch.rankingDifference = -10;
 
-  } else {
-    this.addMatch.rankingDifference;
-  };
-};
+    } else {
+      this.addMatch.rankingDifference;
+    };
+  }
 
-PULSEBALL.prototype.updateTeamScores = function (team1Index, team2Index, rankingDifference, outcome) {
-  switch (outcome) {
-    case 'points A':
-      var points = parseFloat((1 - ( rankingDifference / 10 )).toFixed(2));
-      this.rankingsTable[team1Index].pts += points;
-      this.rankingsTable[team2Index].pts -= points;
-      break;
+  updateTeamScores(team1Index, team2Index, rankingDifference, outcome){
+    switch (outcome) {
+      case 'points A':
+        var points = parseFloat((1 - ( rankingDifference / 10 )).toFixed(2));
+        this.rankingsTable[team1Index].pts += points;
+        this.rankingsTable[team2Index].pts -= points;
+        break;
 
-    case 'B':
-      var points = parseFloat((1 + ( rankingDifference / 10 )).toFixed(2));
-      this.rankingsTable[team1Index].pts -= points;
-      this.rankingsTable[team2Index].pts += points;
-      break;
+      case 'B':
+        var points = parseFloat((1 + ( rankingDifference / 10 )).toFixed(2));
+        this.rankingsTable[team1Index].pts -= points;
+        this.rankingsTable[team2Index].pts += points;
+        break;
 
-    case 'D':
-      var points = parseFloat((rankingDifference / 10).toFixed(2));
-      this.rankingsTable[team1Index].pts += points;
-      this.rankingsTable[team2Index].pts += points;
-      break;
+      case 'D':
+        var points = parseFloat((rankingDifference / 10).toFixed(2));
+        this.rankingsTable[team1Index].pts += points;
+        this.rankingsTable[team2Index].pts += points;
+        break;
 
-    case 'N':
-      var points = 0;
-      this.rankingsTable[team1Index].pts += points;
-      this.rankingsTable[team2Index].pts += points;
-      break;
-  };
-
-};
-
-PULSEBALL.prototype.sortTable = function () {
-  //sort by points
-  this.rankingsTable.sort(function(a, b){
-        return parseFloat(a.pts).toFixed(2) - parseFloat(b.pts).toFixed(2);
-  }).reverse();
-  //then change positions
-  for (let i = 0; i < this.rankingsTable.length; i++) {
-    this.rankingsTable[i].pos = i + 1;
-  };
-};
+      case 'N':
+        var points = 0;
+        this.rankingsTable[team1Index].pts += points;
+        this.rankingsTable[team2Index].pts += points;
+        break;
+    };
+  }
+  
+  sortTable(){
+    //sort by points
+    this.rankingsTable.sort(function(a, b){
+          return parseFloat(a.pts).toFixed(2) - parseFloat(b.pts).toFixed(2);
+    }).reverse();
+    //then change positions
+    for (let i = 0; i < this.rankingsTable.length; i++) {
+      this.rankingsTable[i].pos = i + 1;
+    };
+  }
+}
